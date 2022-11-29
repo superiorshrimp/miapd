b1a5b4e3c787c4938b8208013f143ae6deb0acc7
superior_shrimp <szymon.zychowicz300701@gmail.com>
2022-11-29 17:20:56 +0100

    poprawione drzewo projektu

--- project/src/main/java/gui/App.java
+++ project/src/main/java/gui/App.java
@@ -2,9 +2,9 @@ package gui;
 
 import javafx.stage.Stage;
 
-public class App extends javafx.application.Application{
+public class App extends javafx.application.Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-
+        primaryStage.show();
     }
 }
