d602ad4effcdf891dee96ff983bc22b455b3a352
superior_shrimp <szymon.zychowicz300701@gmail.com>
2022-11-29 17:32:45 +0100

    screeny z upela dodane

--- project/src/main/java/gui/App.java
+++ project/src/main/java/gui/App.java
@@ -1,10 +1,14 @@
 package gui;
 
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class App extends javafx.application.Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.show();
+        primaryStage.setOnCloseRequest((WindowEvent we) -> {
+            System.exit(0);
+        });
     }
 }
