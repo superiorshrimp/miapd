5227fcd32e53ffb7fcd6a8febd18a152fbb0765e
superior_shrimp <szymon.zychowicz300701@gmail.com>
2022-11-29 16:01:06 +0100

    trzeba zbudować dobrze projekt

---
+++ src/gui/App.java
@@ -0,0 +1,10 @@
+package gui;
+
+import javafx.stage.Stage;
+
+public class App extends javafx.application.Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+    }
+}
