7111591eeb7dc81bbdf2d6c02236c99dd3293d0c
Mateusz Wiecek <mateusz.wiecek@interia.eu>
2022-12-28 23:46:57 +0100

    my part for now

--- project/src/main/java/gui/UserModule.java
+++ project/src/main/java/gui/UserModule.java
@@ -13,38 +13,66 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import javafx.util.Pair;
 import main.Phone;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 public class UserModule {
+    private static int stageNumber = 0;
     ArrayList<Label> labels = new ArrayList<>();
+    ArrayList<Pair<VBox, Integer>> vBoxes = new ArrayList<>();
+    float[][] userPreferences;
     public UserModule(ArrayList<String> labels){
         for ( String label : labels){
             this.labels.add(new Label(label));
         }
+        int arraySize = this.labels.size();
+        this.userPreferences = new float[arraySize][arraySize];
+        for (int i = 0; i < arraySize; i++){
+            this.userPreferences[i][i] = 1;
+        }
+        for (int i = 0; i < labels.size()-1; i++) {
+            Label label = this.labels.get(i);
+            VBox vBox = new VBox(label);
+            vBox.setAlignment(Pos.CENTER);
+            vBox.setPadding(new Insets(10, 10, 10, 10));
+            vBoxes.add(new Pair<>(vBox, i*7+i+1));
+            System.out.println(i*7+i+1);
+            System.out.println((i*7+i+1) / labels.size());
+            System.out.println((i*7+i+1) % labels.size());
+            System.out.println();
+        }
+        Collections.shuffle(vBoxes);
+        Label label = this.labels.get(labels.size()-1);
+        VBox vBox = new VBox(label);
+        vBox.setAlignment(Pos.CENTER);
+        vBox.setPadding(new Insets(10, 10, 10, 10));
+        vBoxes.add(new Pair<>(vBox, (labels.size()-1)*7+labels.size()-1+1));
     }
     public void start(){
         Stage userStage = new Stage();
-        ArrayList<VBox> vBoxes = new ArrayList<>();
-        for (int i = 0; i < labels.size(); i++){
-            ChoiceBox<Integer> cb = new ChoiceBox<>(FXCollections.observableArrayList(
-                    1, 2, 3, 4, 5, 6, 7, 8, 9
-            ));
-            VBox vBox = new VBox(labels.get(i), cb);
-            vBox.setAlignment(Pos.CENTER);
-            vBox.setPadding(new Insets(10, 10, 10, 10));
-            vBoxes.add(vBox);
-        }
 
-        HBox hBox = new HBox();
-        for (VBox vBox : vBoxes){
-            hBox.getChildren().add(vBox);
-        }
 
         Button nextButton = new Button("Next");
-        Label infoLabel = new Label("How important to you are");
-        FlowPane flowPane = new FlowPane(infoLabel, hBox, nextButton);
+        Label infoLabel = new Label("Compare importance of first feature to second");
+        HBox hBox = new HBox(vBoxes.get(UserModule.stageNumber).getKey(), vBoxes.get(UserModule.stageNumber + 1).getKey());
+        System.out.println(UserModule.stageNumber);
+        int xIdx = vBoxes.get(UserModule.stageNumber).getValue() / labels.size();
+        int yIdx = vBoxes.get(UserModule.stageNumber).getValue() % labels.size();
+
+        ChoiceBox<Integer> cb = new ChoiceBox<>(FXCollections.observableArrayList(
+                1, 2, 3, 4, 5, 6, 7, 8, 9
+        ));
+
+        hBox.setAlignment(Pos.CENTER);
+        VBox vBox = new VBox(infoLabel, hBox, cb, nextButton);
+        vBox.setAlignment(Pos.CENTER);
+        FlowPane flowPane = new FlowPane(vBox);
 
         flowPane.setAlignment(Pos.CENTER);
         Scene scene = new Scene(flowPane);
@@ -55,7 +83,17 @@ public class UserModule {
 
         nextButton.setOnAction(event -> {
             userStage.close();
-            showResults();
+            UserModule.stageNumber += 1;
+            if (UserModule.stageNumber > labels.size() - 2) calculateResults();
+            else start();
+        });
+
+        cb.setOnAction(event -> {
+            userPreferences[xIdx][yIdx] = cb.getValue();
+            for (var line : this.userPreferences){
+                System.out.println(Arrays.toString(line));
+            }
+            System.out.println();
         });
 
         userStage.setOnCloseRequest((WindowEvent we) -> {
@@ -67,12 +105,40 @@ public class UserModule {
     private Phone getResults(){
         return new Phone("a", 1,1,1,1,1,1,1,1); //temporarily return basic phone
     }
-    public void showResults(){
+
+    private void calculateResults(){
+        float size = this.userPreferences.length;
+        for ( int x = 0; x < size - 2; x ++ ){
+            for ( int y = 2; y < size; y++ ){
+                if (x + y > 6) break;
+                System.out.println("x " + (y-2) + " y " + (x + y));
+                System.out.println("x " + (y-2) + " y " + (x + y -1));
+                System.out.println("x " + (x + y -1) + " y " + (x + y));
+                System.out.println();
+                userPreferences[y - 2][x + y] = Math.min((float) Math.sqrt(userPreferences[y - 2][x + y - 1] * userPreferences[x + y - 1][x + y]), 9);
+            }
+        }
+
+        for ( int x = 0; x < size - 1; x++ ){
+            for ( int y = 1; y < size; y++ ){
+                if (x + y > 6) break;
+                System.out.println("x " + (x + y) + " y " + (y - 1));
+                System.out.println();
+                userPreferences[x + y][y - 1] = 1/userPreferences[y - 1][x + y];
+            }
+        }
+
+        System.out.println();
+        for (var line : this.userPreferences){
+            System.out.println(Arrays.toString(line));
+        }
+        showResults();
+    }
+    private void showResults(){
         Stage resultsStage = new Stage();
         resultsStage.setTitle("Results");
         Phone bestPhone = getResults();
         HBox hBox = new HBox(new Label(bestPhone.toString()));
-
         Scene scene = new Scene(hBox);
         resultsStage.setScene(scene);
         resultsStage.show();
